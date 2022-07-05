e79e235da268f0d7f1e2efb9639c3af247e86982
epicmine99 <92741333+epicmine99@users.noreply.github.com>
2022-07-05 16:12:04 -0700

    added some skeleton code for AppointmentManager and PatientAccess

--- src/useCases/AppointmentManager.java
+++ src/useCases/AppointmentManager.java
@@ -1,4 +1,34 @@
 package useCases;
 
 public class AppointmentManager {
+    public List<Appointment> Appointments;
+
+    public boolean bookAppointment(Patient patient, Doctor doctor, DateTime startTime, DateTime endTime){
+
+        return true;
+    }
+    public void removeAppointment(Patient patient){
+        //only intakes patient as this function will print all appointments for a patient and then remove the one
+        //returned on the console
+    }
+    public boolean rescheduleAppointment(String patient, DateTime newStart, DateTime newEnd){
+        //similar to removeAppointment, prompts the user with all appointments and then changes the datetime values
+        //of the selected appointment
+        return true;
+    }
+    public ArrayList<Appointments> getAppointments(Patient patient){
+
+    }
+    public ArrayList<Appointments> getAppointments(Doctor doctor){
+
+    }
+    public ArrayList<Appointments> getAllAppointments(){
+
+    }
+    public ArrayList<Timeblock> getAvailability(Doctor doctor){
+
+    }
+    public void changeAvailability(Doctor doctor){
+
+    }
 }
