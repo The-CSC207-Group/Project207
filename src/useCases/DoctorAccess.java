eedc38e19bd22bbbfda834d133809dbdf985f7ce
Hepaul7 <paulhe0773@gmail.com>
2022-07-07 18:53:27 +0200

    worked on DoctorAccess

--- src/useCases/DoctorAccess.java
+++ src/useCases/DoctorAccess.java
@@ -1,22 +1,24 @@
-//package useCases;
-//
-//import database.DataMapperGateway;
-//
-//public class DoctorAccess {
-//
-//    private DataMapperGateway<Doctor> doctorDatabase;
-//    private DataMapperGateway<Patient> patientDatabase;
-//    AppointmentManager appointmentManager;
-//
-//    public DoctorAccess(DataMapperGateway<Doctor> doctorDatabase, DataMapperGateway<Patient> patientDatabase,
-//                         AppointmentManager appointmentManager){
-//        this.doctorDatabase = doctorDatabase;
-//        this.patientDatabase = patientDatabase;
-//        this.appointmentManager = appointmentManager;
-//    }
-//    public void signOut(){
-//
-//    }
-//
-//
-//}
+package useCases;
+
+import database.DataMapperGateway;
+import entities.Doctor;
+import entities.Patient;
+
+public class DoctorAccess {
+
+    private DataMapperGateway<Doctor> doctorDatabase;
+    private DataMapperGateway<Patient> patientDatabase;
+    AppointmentManager appointmentManager;
+
+    public DoctorAccess(DataMapperGateway<Doctor> doctorDatabase, DataMapperGateway<Patient> patientDatabase,
+                         AppointmentManager appointmentManager){
+        this.doctorDatabase = doctorDatabase;
+        this.patientDatabase = patientDatabase;
+        this.appointmentManager = appointmentManager;
+    }
+    public void signOut(){
+
+    }
+
+
+}
