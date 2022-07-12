a9cac3c43bc4bd1a14e0db86e93e088233c54cb0
ryanMMM <55326695+ryanMMM@users.noreply.github.com>
2022-07-12 23:49:02 +0400

    Revert "created utilities directory and added utility that deletes files"
    This reverts commit e97d2a7f2b11533a13277cd5721941e9530cc786.

--- src/Utilities/DeleteUtils.java
+++ src/Utilities/DeleteUtils.java
@@ -1,8 +1,8 @@
-/* deleteDirectory method source: https://www.baeldung.com/java-delete-directory*/
-
 package Utilities;
 
 import java.io.File;
+
+/* deleteDirectory method source: https://www.baeldung.com/java-delete-directory*/
 
 public class DeleteUtils {
     public static boolean deleteDirectory(File directoryToBeDeleted) {
