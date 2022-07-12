9a3adb3abcc6649540f5c0988dc7d89e30974b32
Hepaul7 <paulhe0773@gmail.com>
2022-07-13 01:16:58 +0200

    userManager modified create methods

--- src/useCases/UserManager.java
+++ src/useCases/UserManager.java
@@ -18,30 +18,26 @@ public class UserManager {
 
     public boolean createPatient(String username, String password, int contactInfo, String healthNumber){
         Patient patient = new Patient(username, password, contactInfo, healthNumber);
-        patientDatabase.add(patient);
-        HashSet<Integer> all_ids  = patientDatabase.getAllIds();
-        return all_ids.contains(patient.getId());
+        Integer user_id = patientDatabase.add(patient);
+        return user_id != null;
     }
 
     public boolean createDoctor(String username, String password, int contactInfo){
         Doctor doctor = new Doctor(username, password, contactInfo);
-        doctorDatabase.add(doctor);
-        HashSet<Integer> all_ids = doctorDatabase.getAllIds();
-        return all_ids.contains(doctor.getId());
+        Integer user_id = doctorDatabase.add(doctor);
+        return user_id != null;
     }
 
     public boolean createSecretary(String username, String password, int contactInfo){
         Secretary secretary = new Secretary(username, password, contactInfo);
-        secretaryDatabase.add(secretary);
-        HashSet<Integer> all_ids = secretaryDatabase.getAllIds();
-        return all_ids.contains(secretary.getId());
+        Integer user_id = secretaryDatabase.add(secretary);
+        return user_id != null;
     }
 
     public boolean createAdmin(String username, String password, int contactInfo){
         Admin admin = new Admin(username, password, contactInfo);
-        adminDatabase.add(admin);
-        HashSet<Integer> all_ids = adminDatabase.getAllIds();
-        return all_ids.contains(admin.getId());
+        Integer user_id = adminDatabase.add(admin);
+        return user_id != null;
     }
     public void addLogIdToUserLogs(Integer userId, Integer logId){
         User user = getUserWithId(userId);
