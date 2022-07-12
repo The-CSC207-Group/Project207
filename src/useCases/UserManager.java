d3a2b4c0a148082dd1f3e0ed06b8cacd97ff1000
Hepaul7 <paulhe0773@gmail.com>
2022-07-13 00:33:24 +0200

    UserManager

--- src/useCases/UserManager.java
+++ src/useCases/UserManager.java
@@ -1,54 +1,43 @@
-//package useCases;
-//
-//import database.DataMapperGateway;
-//import entities.Log;
-//import entities.User;
-//
-//import java.util.ArrayList;
-//
-//
-//public class UserManager {
-//
-//    private User currentUser;
-//    private DataMapperGateway<User> database;
-//
-//    public UserManager(String username, DataMapperGateway<User> database) {
-//        this.database = database;
-//        currentUser = database.get(username);
-//    }
-//
-//    public User getCurrentUser() {
-//        return currentUser;
-//    }
-//
-//    /**
-//     * Remove current user from database.
-//     */
-//    public void deleteCurrentUser() {
-//        // Not viable for synchronous activity
-//        database.remove(currentUser.getUsername());
-//    }
-//
-//    /**
-//     *
-//     * @return current user's logs as an arraylist of logs.
-//     */
-//    public ArrayList<Log> getCurrentUserLogs() {
-//        return currentUser.getLogs();
-//    }
-//
-//    /**
-//     * Changes the user's password in database.
-//     * @param newPassword new password
-//     */
-//    public void changeCurrentUserPassword(String newPassword) {
-//        currentUser.setPassword(newPassword);
-//    }
-//
-//    /**
-//     * Sets current user to null.
-//     */
-//    public void signOut() {
-//        currentUser = null;
-//    }
-//}
+package useCases;
+
+import database.DataMapperGateway;
+import entities.*;
+
+import java.util.HashSet;
+
+
+public class UserManager {
+
+    DataMapperGateway<Patient> patientDatabase;
+    DataMapperGateway<Doctor> doctorDatabase;
+    DataMapperGateway<Secretary> secretaryDatabase;
+    DataMapperGateway<Admin> adminDatabase;
+
+    public boolean createPatient(String username, String password, int contactInfo, String healthNumber){
+        Patient patient = new Patient(username, password, contactInfo, healthNumber);
+        patientDatabase.add(patient);
+        HashSet<Integer> all_ids  = patientDatabase.getAllIds();
+        return all_ids.contains(patient.getId());
+    }
+
+    public boolean createDoctor(String username, String password, int contactInfo){
+        Doctor doctor = new Doctor(username, password, contactInfo);
+        doctorDatabase.add(doctor);
+        HashSet<Integer> all_ids = doctorDatabase.getAllIds();
+        return all_ids.contains(doctor.getId());
+    }
+
+    public boolean createSecretary(String username, String password, int contactInfo){
+        Secretary secretary = new Secretary(username, password, contactInfo);
+        secretaryDatabase.add(secretary);
+        HashSet<Integer> all_ids = secretaryDatabase.getAllIds();
+        return all_ids.contains(secretary.getId());
+    }
+
+    public boolean createAdmin(String username, String password, int contactInfo){
+        Admin admin = new Admin(username, password, contactInfo);
+        adminDatabase.add(admin);
+        HashSet<Integer> all_ids = adminDatabase.getAllIds();
+        return all_ids.contains(admin.getId());
+    }
+}
