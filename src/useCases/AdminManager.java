9b091bbf15d68be6f3db06b2b472e5258f4f02da
Hepaul7 <paulhe0773@gmail.com>
2022-07-13 01:55:49 +0200

    moved methods from usermanager to managers

--- src/useCases/AdminManager.java
+++ src/useCases/AdminManager.java
@@ -1,4 +1,14 @@
 package useCases;
 
+import database.DataMapperGateway;
+import entities.Admin;
+
 public class AdminManager {
+    DataMapperGateway<Admin> adminDatabase;
+
+    public boolean createAdmin(String username, String password, int contactInfo){
+        Admin admin = new Admin(username, password, contactInfo);
+        Integer user_id = adminDatabase.add(admin);
+        return user_id != null;
+    }
 }
