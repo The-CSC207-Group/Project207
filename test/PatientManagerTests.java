65408b8de3518930530221fec7cb67f7d309aed1
ryanMMM <55326695+ryanMMM@users.noreply.github.com>
2022-07-14 23:01:59 +0400

    Added tests for the patient managers createPatient method

--- test/PatientManagerTests.java
+++ test/PatientManagerTests.java
@@ -1,9 +1,53 @@
+import dataBundles.ContactDataBundle;
+import dataBundles.PatientDataBundle;
+import database.DataMapperGateway;
+import database.Database;
+import entities.Contact;
+import entities.Patient;
+import org.junit.After;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import useCases.managers.PatientManager;
+import utilities.DeleteUtils;
+
+import java.io.File;
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PatientManagerTests {
 
     @Rule
     public TemporaryFolder databaseFolder = new TemporaryFolder();
 
+    @Test(timeout = 1000)
+    public void testCreatePatient() {
+        Database originalDatabase = new Database(databaseFolder.toString());
+        DataMapperGateway<Patient> patientDatabase = originalDatabase.getPatientDatabase();
+        DataMapperGateway<Contact> contactDatabase = originalDatabase.getContactDatabase();
+
+        String username = "jeff";
+        String password = "123";
+        String healthNumber = "5544";
+        Date birthday = new Date(2022, Calendar.JANUARY, 1);
+        ContactDataBundle contactDataBundle = new ContactDataBundle("jeff", "jeff@gmail.com",
+                "12345678", "jeff street", birthday, "jim",
+                "jim@gmail.com", "87654321",
+                "father");
+
+        PatientManager patientManager = new PatientManager(patientDatabase, contactDatabase);
+
+        PatientDataBundle patientDataBundle = patientManager.createPatient(username, password, contactDataBundle,
+                healthNumber);
+
+        /* Testing if the return patient data bundle is valid by testing if the fields of are equal to the parameters of
+        createPatient */
+        assertEquals("The created patient data bundle should have the same name as the parameters of " +
+                        "createPatient method", patientDataBundle.getUsername(), username);
+        assertEquals("The created patient data bundle should have the same health number as the parameters " +
+                "of createPatient method", patientDataBundle.getHealthNumber(), healthNumber);
+    }
 }
