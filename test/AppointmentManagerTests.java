4735cd582d195263d083026cc33bd87b9ba4f319
ryanMMM <55326695+ryanMMM@users.noreply.github.com>
2022-07-14 19:43:17 +0400

    Added testing file appointment managers

---
+++ test/AppointmentManagerTests.java
@@ -0,0 +1,9 @@
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+
+public class AppointmentManagerTests {
+
+    @Rule
+    public TemporaryFolder databaseFolder = new TemporaryFolder();
+
+}
