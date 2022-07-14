aded1e01623795ec619abf099a2a62aa0694f176
ryanMMM <55326695+ryanMMM@users.noreply.github.com>
2022-07-15 00:11:49 +0400

    Created testing file for doctor managers

---
+++ test/DoctorManagerTests.java
@@ -0,0 +1,9 @@
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+
+public class DoctorManagerTests {
+
+    @Rule
+    public TemporaryFolder databaseFolder = new TemporaryFolder();
+
+}
