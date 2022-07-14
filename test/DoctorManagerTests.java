b85a44f1ac8d51876e0983b1a34ffba3b1756a08
ryanMMM <55326695+ryanMMM@users.noreply.github.com>
2022-07-15 00:13:53 +0400

    Added a JUnit after method to DoctorManagerTests that deletes the temporary folders after the tests

--- test/DoctorManagerTests.java
+++ test/DoctorManagerTests.java
@@ -1,9 +1,18 @@
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.rules.TemporaryFolder;
+import utilities.DeleteUtils;
+
+import java.io.File;
 
 public class DoctorManagerTests {
 
     @Rule
     public TemporaryFolder databaseFolder = new TemporaryFolder();
+
+    @After
+    public void after() {
+        DeleteUtils.deleteDirectory(new File(databaseFolder.toString()));
+    }
 
 }
