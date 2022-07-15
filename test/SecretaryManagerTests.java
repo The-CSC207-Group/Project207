2ee13c0bb191a25426f868d5ece60ef0a3e49225
ryanMMM <55326695+ryanMMM@users.noreply.github.com>
2022-07-15 19:22:53 +0400

    Added tests for the secretary manager createSecretary method

--- test/SecretaryManagerTests.java
+++ test/SecretaryManagerTests.java
@@ -1,14 +1,60 @@
+import dataBundles.ContactDataBundle;
+import dataBundles.SecretaryDataBundle;
+import database.DataMapperGateway;
+import database.Database;
+import entities.Contact;
+import entities.Secretary;
 import org.junit.After;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import useCases.managers.SecretaryManager;
 import utilities.DeleteUtils;
 
 import java.io.File;
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SecretaryManagerTests {
 
     @Rule
     public TemporaryFolder databaseFolder = new TemporaryFolder();
+
+    @Test(timeout = 1000)
+    public void testCreateSecretary() {
+        Database originalDatabase = new Database(databaseFolder.toString());
+        DataMapperGateway<Secretary> secretaryDatabase = originalDatabase.getSecretaryDatabase();
+        DataMapperGateway<Contact> contactDatabase = originalDatabase.getContactDatabase();
+
+        String username = "jeff";
+        String password = "123";
+        LocalDate birthday = LocalDate.of(2022, 1, 1);
+        ContactDataBundle contactDataBundle = new ContactDataBundle("jeff", "jeff@gmail.com",
+                "12345678", "jeff street", birthday, "jim",
+                "jim@gmail.com", "87654321",
+                "father");
+
+        SecretaryManager secretaryManager = new SecretaryManager(secretaryDatabase, contactDatabase);
+
+        SecretaryDataBundle secretaryDataBundle = secretaryManager.createSecretary(username, password,
+                contactDataBundle);
+
+        /* Testing if the return secretary data bundle is valid by testing if the fields of are equal to the parameters of
+        createSecretary */
+        assertEquals("The created secretary data bundle should have the same name as the parameters of " +
+                "createSecretary method", secretaryDataBundle.getUsername(), username);
+
+        Secretary loadedSecretary = secretaryDatabase.get(secretaryDataBundle.getId());
+
+        /* Testing if the secretary object has been correctly added to the database by testing if the fields of the loaded
+        secretary are equal to the parameters of createSecretary */
+        assertEquals("Original secretary and loaded secretary should share the same unique username",
+                loadedSecretary.getUsername(), username);
+        assertTrue("Original secretary and loaded secretary should share the same password",
+                loadedSecretary.comparePassword(password));
+    }
 
     @After
     public void after() {
