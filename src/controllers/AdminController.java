0aae3c4de9530f8a5b51cc19ce2bfba585fad933
HridanshKhaitan <h.khaitan@mail.utoronto.ca>
2022-07-16 23:30:16 +0530

    Created AdminController

---
+++ src/controllers/AdminController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+public class AdminController extends TerminalController{
+    public AdminController(Context parent) {
+        super(parent);
+    }
+
+    @Override
+    void WelcomeMessage() {
+
+    }
+}
