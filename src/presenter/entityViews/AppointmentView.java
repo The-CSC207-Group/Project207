3786690b6f96b3bc75663a3ed6c1d79b7b1d7a2f
ahmedkhalf <ahmedkhalf567@gmail.com>
2022-07-16 00:43:21 -0400

    Refactor AppointmentView

--- src/presenter/entityViews/AppointmentView.java
+++ src/presenter/entityViews/AppointmentView.java
@@ -7,16 +7,23 @@ import java.util.List;
 
 
 public class AppointmentView {
+
     public String view(Appointment item) {
+        // TODO not a fan of plus notation, use StringBuilder
         TimeBlock timeBlock = item.getTimeBlock();
         return "Appointment starting at " + timeBlock.getStartTime() + " and ending at " + timeBlock.getEndTime()
                 + ".";
     }
 
     public String viewFromList(List<Appointment> items) {
+        // I'm sure similar code to this will be used throughout the entity views
+        // TODO if the need arises, abstract this to a helper or common super class
         StringBuilder appendedOutput = new StringBuilder("");
-        for (Appointment item : items) {
-            appendedOutput.append(view(item)).append("\n");
+        for (int i = 0; i < items.size(); i++) {
+            if (i != items.size() - 1) {
+                appendedOutput.append(view(items.get(i)));
+                appendedOutput.append("\n");
+            }
         }
         return appendedOutput.toString();
     }
