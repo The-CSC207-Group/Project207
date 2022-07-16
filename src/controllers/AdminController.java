8d49664494b40365d2e9ded62ba732646962edb0
HridanshKhaitan <h.khaitan@mail.utoronto.ca>
2022-07-16 23:36:03 +0530

    Created AdminController

--- src/controllers/AdminController.java
+++ src/controllers/AdminController.java
@@ -1,12 +1,37 @@
 package controllers;
 
+import dataBundles.AdminData;
+import dataBundles.SecretaryData;
+import useCases.accessClasses.AdminAccess;
+import useCases.accessClasses.SecretaryAccess;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class AdminController extends TerminalController{
-    public AdminController(Context parent) {
+    private AdminAccess adminAccess;
+    private AdminData adminData;
+    public AdminController(Context parent, AdminData adminData) {
         super(parent);
+        this.adminData = adminData;
+        this.adminAccess = new AdminAccess(getDatabase());
+    }
+    @Override
+    public HashMap<String, Command> AllCommands() {
+        HashMap commands = super.AllCommands();
+        commands.put("Create Secretary Account", new CreateSecretaryAccount());
+        return commands;
     }
 
     @Override
     void WelcomeMessage() {
 
+    }
+    class CreateSecretaryAccount implements Command{
+
+        @Override
+        public boolean execute(ArrayList<String> args) {
+            return false;
+        }
     }
 }
