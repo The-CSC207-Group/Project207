255e8bd22dd082433e8046b039a32a13fcc5ffc3
ryanMMM <55326695+ryanMMM@users.noreply.github.com>
2022-07-16 19:32:58 +0400

    Stored the data for view and get commands in the patient controller

--- src/controllers/PatientController.java
+++ src/controllers/PatientController.java
@@ -1,14 +1,58 @@
 package controllers;
 
-import entities.Patient;
-import entities.User;
+import dataBundles.AppointmentDataBundle;
+import dataBundles.LogDataBundle;
+import dataBundles.PatientDataBundle;
+import dataBundles.PrescriptionDataBundle;
+import presenter.screenViews.PatientView;
+import useCases.accessClasses.PatientAccess;
+
+import java.util.ArrayList;
 
 public class PatientController extends TerminalController {
-    private Patient patient;
+    private PatientAccess patientAccess;
+    private PatientView patientView;
+    private PatientDataBundle patientData;
 
-    public PatientController(Context context, Patient patient) {
+    public PatientController(Context context, PatientDataBundle patientData) {
         super(context);
-        this.patient = patient;
+        this.patientAccess = new PatientAccess(getDatabase());
+        this.patientData = patientData;
+    }
+
+    class ViewAppointments implements Command {
+
+        @Override
+        public boolean execute(ArrayList<String> args) {
+            ArrayList<AppointmentDataBundle> appointments = patientAccess.getAppointments(patientData.getId());
+            return false;
+        }
+    }
+
+    class ViewAllPrescriptions implements Command {
+
+        @Override
+        public boolean execute(ArrayList<String> args) {
+            ArrayList<PrescriptionDataBundle> prescriptions = patientAccess.getAllPrescriptions(patientData.getId());
+            return false;
+        }
+    }
+
+    class ViewActivePrescriptions implements Command {
+        @Override
+        public boolean execute(ArrayList<String> args) {
+            ArrayList<PrescriptionDataBundle> prescriptions = patientAccess.getActivePrescriptions(patientData.getId());
+            return false;
+        }
+    }
+
+    class GetLogs implements Command{
+
+        @Override
+        public boolean execute(ArrayList<String> args) {
+            ArrayList<LogDataBundle> logs = patientAccess.getLogs(patientData.getUsername());
+            return false;
+        }
     }
 
     @Override
