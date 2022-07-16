92e381616c53eaa1122829a3290e4a423d478052
HridanshKhaitan <h.khaitan@mail.utoronto.ca>
2022-07-16 14:43:17 +0530

    Changes to SecretaryController

--- src/controllers/SecretaryController.java
+++ src/controllers/SecretaryController.java
@@ -1,10 +1,12 @@
 package controllers;
 
+import dataBundles.ContactDataBundle;
 import dataBundles.SecretaryDataBundle;
 import entities.Secretary;
 import useCases.accessClasses.DoctorAccess;
 import useCases.accessClasses.SecretaryAccess;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class SecretaryController extends TerminalController {
@@ -22,11 +24,52 @@ public class SecretaryController extends TerminalController {
     @Override
     public HashMap<String, Command> AllCommands() {
         HashMap commands = super.AllCommands();
+        commands.put("changePassword", new ChangePassword());
+        commands.put("createPatientAccount", new CreatePatientAccount());
+        commands.put("createDoctorAccount", new CreateDoctorAccount());
         return commands;
     }
 
     @Override
     void WelcomeMessage() {
 
+    }
+
+    class CreatePatientAccount implements Command{
+
+        @Override
+        public boolean execute(ArrayList<String> args) {
+            return false;
+        }
+    }
+    class CreateDoctorAccount implements Command{
+
+        @Override
+        public boolean execute(ArrayList<String> args) {
+            String username = presenter.promptPopup("Enter Username");
+            String password = presenter.promptPopup("Enter Password");
+            ContactDataBundle contact;
+//            if (){
+//                secretaryAccess.createDoctor(username, password, contact);// need to implement error or success message
+//                presenter.successMessage("Successfully created new doctor");}
+//            else {
+//                presenter.warningMessage("This username already exists. No new doctor account created");}
+            return false;
+        }
+    }
+    class ChangePassword implements Command{
+
+        @Override
+        public boolean execute(ArrayList<String> args) {
+            String p1 = presenter.promptPopup("Enter New Password");
+            String p2 = presenter.promptPopup("Re-enter new password");
+            if (p1.equals(p2)){
+                secretaryAccess.changePassword(secretaryDataBundle.getId(), p1 );
+                presenter.successMessage("Successfully changed password");
+            } else {
+                presenter.errorMessage("Invalid! Please ensure both passwords match");
+            }
+            return false;
+        }
     }
 }
