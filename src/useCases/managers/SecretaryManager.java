045a3bf9461fafdc12d558ef486be4776a4ff33d
Hepaul7 <paulhe0773@gmail.com>
2022-07-16 22:48:46 +0200

    Javadocs for SecretaryManager

--- src/useCases/managers/SecretaryManager.java
+++ src/useCases/managers/SecretaryManager.java
@@ -5,7 +5,7 @@ import dataBundles.SecretaryData;
 import database.DataMapperGateway;
 import entities.*;
 
-public class SecretaryManager extends UserManager<Secretary>{
+public class SecretaryManager extends UserManager<Secretary> {
 
     DataMapperGateway<Secretary> secretaryDatabase;
     DataMapperGateway<Contact> contactDatabase;
@@ -14,18 +14,26 @@ public class SecretaryManager extends UserManager<Secretary>{
 
     /**
      * Stores the databases.
+     *
      * @param secretaryDatabase DataMapperGateway<Secretary>
-     * @param contactDatabase DataMapperGateway<Contact>
+     * @param contactDatabase   DataMapperGateway<Contact>
      */
 
-    public SecretaryManager(DataMapperGateway<Secretary> secretaryDatabase, DataMapperGateway<Contact> contactDatabase){
+    public SecretaryManager(DataMapperGateway<Secretary> secretaryDatabase, DataMapperGateway<Contact> contactDatabase) {
         super(secretaryDatabase);
         this.secretaryDatabase = secretaryDatabase;
         this.secretaryMethods = new GenericUserManagerMethods<>(secretaryDatabase);
         this.contactDatabase = contactDatabase;
     }
 
-    public SecretaryData createSecretary(String username, String password){
+    /**
+     * Creates a new secretary and stores it in the database.
+     *
+     * @param username String username of the new secretary, cannot exist in the database yet.
+     * @param password String password for the user.
+     * @return Secretary Data consisting all information for this secretary.
+     */
+    public SecretaryData createSecretary(String username, String password) {
         Secretary secretary = new Secretary(username, password);
         secretaryDatabase.add(secretary);
         return new SecretaryData(secretary);
