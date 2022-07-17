8490456ccd58dae1b064752683dc01b7ae4d6d74
HridanshKhaitan <h.khaitan@mail.utoronto.ca>
2022-07-17 23:52:19 +0530

    Changes to SecretaryController

--- src/controllers/SecretaryLoadedPatientController.java
+++ src/controllers/SecretaryLoadedPatientController.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 
 public class SecretaryLoadedPatientController extends TerminalController{
     PatientData patientData;
+
     SecretaryData secretaryData;
     SecretaryAccess secretaryAccess;
     SecretaryController secretaryController;
