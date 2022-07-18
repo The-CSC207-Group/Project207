7f4e2a0f2786826b604c82e14aafcc63af1092f3
ryanMMM <55326695+ryanMMM@users.noreply.github.com>
2022-07-18 23:10:13 +0400

    Updated the patient controller's change password and get logs commands to use presenter methods

--- src/controllers/PatientController.java
+++ src/controllers/PatientController.java
@@ -3,6 +3,7 @@ package controllers;
 import dataBundles.AppointmentData;
 import dataBundles.LogData;
 import dataBundles.PatientData;
+import presenter.response.PasswordResetDetails;
 import presenter.screenViews.PatientScreenView;
 import presenter.screenViews.SecretaryScreenView;
 import useCases.accessClasses.PatientAccess;
@@ -40,12 +41,11 @@ public class PatientController extends TerminalController {
 
         @Override
         public void execute(ArrayList<String> args) {
-            String newPassword1 = presenter.promptPopup("Enter a new password");
-            String newPassword2 = presenter.promptPopup("Re-enter the new password");
-            if (newPassword1.equals(newPassword2)){
-                patientAccess.changeCurrentUserPassword(patientData, newPassword1);
+            PasswordResetDetails passwordResetDetails = patientScreenView.resetPasswordPrompt();
+            if (passwordResetDetails.password().equals(passwordResetDetails.confirmedPassword())) {
+                patientAccess.changeCurrentUserPassword(patientData, passwordResetDetails.password());
             } else {
-                presenter.errorMessage("These do not match");
+                patientScreenView.showResetPasswordMismatchError();
             }
         }
     }
@@ -72,7 +72,7 @@ public class PatientController extends TerminalController {
         @Override
         public void execute(ArrayList<String> args) {
             ArrayList<LogData> logs = patientAccess.getLogs(patientData);
-            patientScreenView.viewLogs(logs);
+            patientScreenView.viewUserLogs(logs);
         }
     }
 }
