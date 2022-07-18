a86c30ca116672648f96c5b9449548cff53dd283
HridanshKhaitan <h.khaitan@mail.utoronto.ca>
2022-07-18 23:32:56 +0530

    ChangePassword method in AdminController added

--- src/controllers/AdminController.java
+++ src/controllers/AdminController.java
@@ -116,21 +116,7 @@ public class AdminController extends TerminalController{
                 presenter.errorMessage("Invalid! Please ensure both passwords match");
             }};
     }
-    class ChangeAdminPassword implements Command{
 
-        @Override
-        public void execute(ArrayList<String> args) {
-            String p1 = presenter.promptPopup("Enter New Password");
-            String p2 = presenter.promptPopup("Re-enter new password");
-            if (p1.equals(p2)){
-                adminAccess.changePassword(adminData.getUsername(), p1);
-                presenter.successMessage("Successfully changed password");
-            }
-            else {
-                presenter.errorMessage("Invalid! Please ensure both passwords match");
-            }
-        }
-    }
     class getLogs implements Command{
 
         @Override
