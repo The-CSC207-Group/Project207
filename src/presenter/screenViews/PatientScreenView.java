83e2f6490fe9e3ca5dacc6da06fbeffc0ebe3abc
BaraKharseh <bkharseh@gmail.com>
2022-07-18 13:03:52 -0400

    Moved viewAllLogs to UserScreenView from PatientScreenView

--- src/presenter/screenViews/PatientScreenView.java
+++ src/presenter/screenViews/PatientScreenView.java
@@ -44,15 +44,4 @@ public class PatientScreenView extends UserScreenView {
         infoMessage("Booked Appointments:");
         viewAppointments(items);
     }
-
-    public void viewLogs(List<LogData> items) {
-        LogView logView = new LogView();
-        String output = logView.viewFullFromList(items);
-        infoMessage(output);
-    }
-
-    public void viewAllLogs(List<LogData> items) {
-        infoMessage("All Logs:");
-        viewLogs(items);
-    }
 }
--- src/presenter/screenViews/UserScreenView.java
+++ src/presenter/screenViews/UserScreenView.java
@@ -1,6 +1,10 @@
 package presenter.screenViews;
 
+import dataBundles.LogData;
+import presenter.entityViews.LogView;
 import presenter.response.PasswordResetDetails;
+
+import java.util.List;
 
 public abstract class UserScreenView extends ScreenView{
 
@@ -13,5 +17,16 @@ public abstract class UserScreenView extends ScreenView{
 
     public void showResetPasswordMismatchError() {
         errorMessage("Cannot reset password: new password and confirmed new password do not match.");
+    }
+
+    public void viewLogs(List<LogData> items) {
+        LogView logView = new LogView();
+        String output = logView.viewFullFromList(items);
+        infoMessage(output);
+    }
+
+    public void viewAllLogs(List<LogData> items) {
+        infoMessage("All Logs:");
+        viewLogs(items);
     }
 }
