be49ce4e837d12ae13b05813833df5b7bbf0189c
Hepaul7 <paulhe0773@gmail.com>
2022-07-19 00:40:30 +0200

    fixed method call

--- src/controllers/SecretaryController.java
+++ src/controllers/SecretaryController.java
@@ -99,7 +99,7 @@ public class SecretaryController extends TerminalController {
 
     private Command GetLogs() {
         return (x) -> {
-            ArrayList<LogData> logs = logManager.getUserLogs(secretaryData);
+            ArrayList<LogData> logs = logManager.getLogDataBundlesFromUserDataBundle(secretaryData);
             secretaryScreenView.viewUserLogs(logs);
         };
     }
