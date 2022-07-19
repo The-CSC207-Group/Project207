8ae385556477c331b680336691edc72fd1622f25
ahmedkhalf <ahmedkhalf567@gmail.com>
2022-07-19 14:35:15 -0400

    Add javadocs for TerminalScreenView

--- src/presenter/screenViews/TerminalScreenView.java
+++ src/presenter/screenViews/TerminalScreenView.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public class TerminalScreenView extends ScreenView {
 
+    /**
+     * Shows list of available command.
+     * @param helpCommands List of strings representing command
+     */
     public void showHelpView(List<String> helpCommands) {
         infoMessage("Available Commands:");
         for (String command : helpCommands) {
@@ -11,10 +15,18 @@ public class TerminalScreenView extends ScreenView {
         }
     }
 
+    /**
+     * Ask user to input command.
+     * @return string representing stripped input.
+     */
     public String showCommandPrompt() {
         return input(">>> ");
     }
 
+    /**
+     * Display error when the command user types is not in command list.
+     * @param command A command represented as a string.
+     */
     public void showInvalidCommandError(String command) {
         errorMessage("Invalid command: " + command);
     }
