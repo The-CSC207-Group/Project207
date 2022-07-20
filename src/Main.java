dca1367f321f7b14714ba7f4e7c3b22020f3b9d1
epicmine99 <92741333+epicmine99@users.noreply.github.com>
2022-07-20 12:59:48 -0700

    Actually implementing TimeBlockData

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
 import controllers.Context;
 import database.Database;
 import entities.Admin;
+import entities.Availability;
 import entities.Clinic;
+import entities.TimeBlock;
 
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 public class Main {
     public static void main(String[] args) {
 //        DataMapperGateway<User> userDatabase = new UserJsonDatabase();
         Database database = new Database();
-        database.setClinic(new Clinic("", "", "", ZoneId.of("US/Eastern"), null));
+        database.setClinic(new Clinic("", "", "", ZoneId.of("US/Eastern"),
+                new TimeBlock(ZonedDateTime.of(1, 2, 3, 4, 5, 6, 7,
+                        ZoneId.of("US/Eastern")), ZonedDateTime.of(1, 2, 3, 4,
+                        5, 6, 7, ZoneId.of("US/Eastern")))));
         Context c = new Context(database);
         c.run();
         database.getAdminDatabase().add(new Admin("root", "root"));
