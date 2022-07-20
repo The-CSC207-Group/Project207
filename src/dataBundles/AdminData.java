1930de3531fac4a22c6202b195bc949fff815756
DanielDervishi <58835213+DanielDervishi@users.noreply.github.com>
2022-07-20 13:44:38 -0400

    - Finished Javadocs for AdminData, AppointmentData, AvailabilityData and ClinicData.

--- src/dataBundles/AdminData.java
+++ src/dataBundles/AdminData.java
@@ -2,8 +2,15 @@ package dataBundles;
 
 import entities.Admin;
 
+/**
+ * Wrapper class for admin entity.
+ */
 public class AdminData extends UserData<Admin> {
 
+    /**
+     * Constructor for adminData data bundle.
+     * @param admin Admin - admin entity.
+     */
     public AdminData(Admin admin) {
         super(admin);
     }
