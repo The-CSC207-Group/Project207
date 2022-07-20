2385d667209f9b82249d01f2f31b450cf9fb680e
solomon-the-wise <99106363+solomon-the-wise@users.noreply.github.com>
2022-07-20 15:48:08 -0400

    bootstrapped clinic into the database

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 import controllers.Context;
 import database.Database;
 import entities.Admin;
+import entities.Clinic;
+
+import java.time.ZoneId;
 
 public class Main {
     public static void main(String[] args) {
 //        DataMapperGateway<User> userDatabase = new UserJsonDatabase();
         Database database = new Database();
+        database.setClinic(new Clinic("", "", "", ZoneId.of("us")));
         Context c = new Context(database);
         c.run();
         database.getAdminDatabase().add(new Admin("root", "root"));
