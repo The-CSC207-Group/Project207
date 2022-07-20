f0313a27035261bb7af677cabbf9bf71f4a8ddb9
BaraKharseh <bkharseh@gmail.com>
2022-07-20 15:05:52 -0400

    Finished the rest of the entity view Javadocs (except ViewMethod interface)

--- src/presenter/entityViews/SecretaryView.java
+++ src/presenter/entityViews/SecretaryView.java
@@ -4,13 +4,24 @@ import dataBundles.SecretaryData;
 
 import java.util.List;
 
+/**
+ * The Secretary entity's view.
+ */
 public class SecretaryView extends EntityView<SecretaryData> {
 
+    /**
+     * @param item The secretary data bundle to view.
+     * @return Returns item's full secretary view.
+     */
     @Override
     public String viewFull(SecretaryData item) {
         return viewUsername(item);
     }
 
+    /**
+     * @param item The secretary data bundle to view.
+     * @return Returns the secretary's username as a view.
+     */
     public String viewUsername(SecretaryData item) {
         return "Secretary username is " + item.getUsername() + ".";
     }
--- src/presenter/entityViews/TimeBlockView.java
+++ src/presenter/entityViews/TimeBlockView.java
@@ -4,18 +4,32 @@ import dataBundles.TimeBlockData;
 
 import java.util.List;
 
+/**
+ * The TimeBlock entity's view.
+ */
 public class TimeBlockView extends EntityView<TimeBlockData> {
 
+    /**
+     * @param item The time block data bundle to view.
+     * @return Returns item's full time block view.
+     */
     @Override
     public String viewFull(TimeBlockData item) {
         return viewStartTime(item) + "\n"
                 + viewEndTime(item);
     }
 
+    /**
+     * @param item The time block data bundle to view.
+     * @return Returns the time block's start time as a view.
+     */
     public String viewStartTime(TimeBlockData item) {
         return "This time block's start time is " + item.getStartTime() + ".";
     }
-
+    /**
+     * @param item The time block data bundle to view.
+     * @return Returns the time block's end time as a view.
+     */
     public String viewEndTime(TimeBlockData item) {
         return "This time block's end time is " + item.getEndTime() + ".";
     }
