183b4fd5d89852c58340a0e4693e15b754e3e1ed
ahmedkhalf <ahmedkhalf567@gmail.com>
2022-07-21 16:11:58 -0400

    Fix issue 16

--- src/presenter/entityViews/AppointmentView.java
+++ src/presenter/entityViews/AppointmentView.java
@@ -2,8 +2,6 @@ package presenter.entityViews;
 
 import dataBundles.AppointmentData;
 import dataBundles.TimeBlockData;
-import database.Database;
-import entities.TimeBlock;
 
 /**
  * The Appointment entity's view.
@@ -17,9 +15,6 @@ public class AppointmentView extends EntityView<AppointmentData> {
     @Override
     public String viewFull(AppointmentData item) {
         TimeBlockData timeBlock = item.getTimeBlock();
-        Database database = new Database();
-        String doctorUsername = database.getDoctorDatabase().get(item.getDoctorId()).getUsername();
-        return "Appointment starting at " + timeBlock.getStartTime() + " and ending at " + timeBlock.getEndTime()
-                + "with Dr." + doctorUsername + ".";
+        return "Appointment starting at " + timeBlock.getStartTime() + " and ending at " + timeBlock.getEndTime() + ".";
     }
 }
