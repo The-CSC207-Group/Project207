aa3bf12f515336dc0e15c55ddcd8fd8ba03ccdad
ryanMMM <55326695+ryanMMM@users.noreply.github.com>
2022-07-21 22:23:43 +0400

    refactored time manager to be a utility class

--- src/utilities/TimeUtils.java
+++ src/utilities/TimeUtils.java
@@ -1,14 +1,13 @@
-package useCases.managers;
+package utilities;
 
 import entities.TimeBlock;
 
-import java.sql.Time;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
-public class TimeManager {
+public class TimeUtils {
 
     public TimeBlock createTimeBlock(Integer year, Integer month, Integer day, Integer hour, Integer minute, Integer lenOfAppointment){
         ZonedDateTime starTime = createZonedDataTime(year, month, day, hour, minute);
@@ -21,21 +20,27 @@ public class TimeManager {
         }
         return proposedTime;
     }
+
     public LocalDate createLocalDate(Integer year, Integer month, Integer dayOfMonth){
         return LocalDate.of(year, month, dayOfMonth);
     }
+
     public LocalTime createLocalTime(Integer hour, Integer minute, Integer second){
         return LocalTime.of(hour, minute, second);
     }
+
     public ZonedDateTime createZonedDataTime(Integer year, Integer month, Integer dayOfMonth, Integer hour,
                                              Integer minute){
         return ZonedDateTime.of(year, month, dayOfMonth, hour,
                 minute, 0, 0, ZoneId.of("US/Eastern"));
     }
+
     public ZonedDateTime getCurrentZonedDateTime(){
         return ZonedDateTime.now(ZoneId.of("US/Eastern"));
     }
+
     public LocalTime getCurrentLocalTime(){
         return LocalTime.now();
     }
+
 }
