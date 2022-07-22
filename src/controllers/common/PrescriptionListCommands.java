dabe577d11136556eeffc3790ede51e7dfece34a
ryanMMM <55326695+ryanMMM@users.noreply.github.com>
2022-07-22 21:22:20 +0400

    added java docs to prescription list commands

--- src/controllers/common/PrescriptionListCommands.java
+++ src/controllers/common/PrescriptionListCommands.java
@@ -10,17 +10,31 @@ import useCases.managers.PrescriptionManager;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Class consisting of commands relating to listing prescriptions
+ */
 public class PrescriptionListCommands {
 
     private final PatientData patientData;
     private final PatientScreenView patientScreenView = new PatientScreenView();
     private final PrescriptionManager prescriptionManager;
 
+    /**
+     * Initializes the prescription list commands class
+     * @param database Database - collection of all entity databases in the program
+     * @param patientData PatientData - data bundle consisting of the information of the patient whose prescriptions
+     *                    are to be listed by this class
+     */
     public PrescriptionListCommands(Database database, PatientData patientData) {
         this.patientData = patientData;
         this.prescriptionManager = new PrescriptionManager(database);
     }
 
+    /**
+     * Creates a hashmap of all string representations of prescription list commands mapped to the method that each
+     * command calls.
+     * @return HashMap<String, Command> - HashMap of strings mapped to their respective prescription list commands.
+     */
     public HashMap<String, Command> AllCommands() {
         HashMap<String, Command> commands = new HashMap<>();
         commands.put("active prescriptions", new ViewPrescriptions(true, false));
@@ -30,7 +44,7 @@ public class PrescriptionListCommands {
         return commands;
     }
 
-    class ViewPrescriptions implements Command {
+    private class ViewPrescriptions implements Command {
         private final boolean active;
         private final boolean detail;
 
