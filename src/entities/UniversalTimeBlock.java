1420ee32566a2867ed13d763a0863d105850b501
solomon-the-wise <99106363+solomon-the-wise@users.noreply.github.com>
2022-08-08 13:21:07 -0400

    added java doc to Timeblock interfaces

--- src/entities/UniversalTimeBlock.java
+++ src/entities/UniversalTimeBlock.java
@@ -3,6 +3,10 @@ package entities;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 
+/**
+ * interface that provides methods relating to getting the time and date of timeblocks such as appointment and
+ * availability
+ */
 public interface UniversalTimeBlock {
     /**
      * returns the start time of the timeblock
--- src/entities/UniversalTimeBlockWithDay.java
+++ src/entities/UniversalTimeBlockWithDay.java
@@ -3,6 +3,9 @@ package entities;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 
+/**
+ * has methods relating to getting time block information such as start time and date.
+ */
 public interface UniversalTimeBlockWithDay extends UniversalTimeBlock {
     /**
      * returns the date of the time block
