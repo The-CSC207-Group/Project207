b3c35cedfc0581f16f1ae0db6be7747e16798fff
solomon-the-wise <99106363+solomon-the-wise@users.noreply.github.com>
2022-08-08 13:03:31 -0400

    finished adding comments to Time block interfaces

--- src/entities/UniversalTimeBlock.java
+++ src/entities/UniversalTimeBlock.java
@@ -6,15 +6,20 @@ import java.time.LocalTime;
 public interface UniversalTimeBlock {
     /**
      * returns the start time of the timeblock
-     * @return
+     * @return LocalTime
      */
     LocalTime startTime();
 
     /**
      * returns the end time of the timeblock
-     * @return
+     * @return LocalTime
      */
     LocalTime endTime();
+
+    /**
+     * returns the day of week of the appointment
+     * @return LocalTime
+     */
     DayOfWeek dayOfWeek();
 
 }
--- src/entities/UniversalTimeBlockWithDay.java
+++ src/entities/UniversalTimeBlockWithDay.java
@@ -4,10 +4,20 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 public interface UniversalTimeBlockWithDay extends UniversalTimeBlock {
+    /**
+     * returns the date of the time block
+     * @return LocalDate
+     */
     LocalDate date();
 
+    /**
+     *
+     * {@inheritDoc}
+     */
     @Override
     default DayOfWeek dayOfWeek(){
         return date().getDayOfWeek();
     }
+
+
 }
