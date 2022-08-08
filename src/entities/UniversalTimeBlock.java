8fd2e92d646161c42ae0c4c816e0eae96b11350a
solomon-the-wise <99106363+solomon-the-wise@users.noreply.github.com>
2022-08-08 12:52:32 -0400

    started adding javadocs

--- src/entities/UniversalTimeBlock.java
+++ src/entities/UniversalTimeBlock.java
@@ -4,7 +4,16 @@ import java.time.DayOfWeek;
 import java.time.LocalTime;
 
 public interface UniversalTimeBlock {
+    /**
+     * returns the start time of the timeblock
+     * @return
+     */
     LocalTime startTime();
+
+    /**
+     * returns the end time of the timeblock
+     * @return
+     */
     LocalTime endTime();
     DayOfWeek dayOfWeek();
 
